b2f3d0d8eabd7bf98367009c2044a288c74ceccc
DimaProggramer <135269406+DimaProggramer@users.noreply.github.com>
2024-04-24 00:22:58 +0300

    Update usebot_v2.py
    Im not idiot yay

--- usebot_v2.py
+++ usebot_v2.py
@@ -1,3 +1,8 @@
+from pyrogram import Client, filters
+api_id = hey_type_here_yourapi
+api_hash "pleasekillme"
+app = Client("MyAccountSession", api_id, api_hash)
+
 text = ""
 
 @app.on_message(filters.command("spam", prefixes='.') & filters.me)
@@ -6,3 +11,4 @@ async def spam(client, message):
         await app.send_message(message.chat.id, text)
 
 # im idiot N1
+app.run()
